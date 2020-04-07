cee94765d6644985f2799f677f7d97dbed4ca21b
smurphy2230 <smurphy2230@gmail.com>
2020-04-06 20:44:21 -0400

    fixed mistaken link for js

--- ch10ex1.js
+++ ch10ex1.js
@@ -1,16 +1,15 @@
-var myImages = [
+var myImages = [ 
     "images/usa.png",
     "images/canada.png",
     "images/jamaica.png",
     "images/mexico.png"
 ];
-
 function changeImg(that) {
     var newImgNumber = Math.round(Math.random() * 3);
 
     while (that.src.indexOf(myImages[newImgNumber]) != -1) {
         newImgNumber = Math.round(Math.random() * 3);
-
+    
     }
 
     that.src = myImages[newImgNumber];
