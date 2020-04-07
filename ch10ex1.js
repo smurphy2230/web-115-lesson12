4511502d7917b278d4fc5045ce95802a82bca8f8
smurphy2230 <smurphy2230@gmail.com>
2020-04-06 20:35:14 -0400

    sometimes the frustration is enough

---
+++ ch10ex1.js
@@ -0,0 +1,17 @@
+var myImages = [
+    "images/usa.png",
+    "images/canada.png",
+    "images/jamaica.png",
+    "images/mexico.png"
+];
+
+function changeImg(that) {
+    var newImgNumber = Math.round(Math.random() * 3);
+
+    while (that.src.indexOf(myImages[newImgNumber]) != -1) {
+        newImgNumber = Math.round(Math.random() * 3);
+
+    }
+
+    that.src = myImages[newImgNumber];
+}
