42877bb177c189410fdb62168dc113363cf2f877
smurphy2230 <smurphy2230@gmail.com>
2020-04-09 10:23:25 -0400

    hmmmm

--- lab12/Chapter7ProjectDataFiles/calculate_coins.js
+++ lab12/Chapter7ProjectDataFiles/calculate_coins.js
@@ -1,3 +1,14 @@
 var $ = function (id) {
     return document.getElementById(id);
 }
+
+let cents = parseInt(document.getElementById("cents").value);
+function generateCoinChange() {
+    let quarter = 0;
+    let dime = 0;
+    let nickel = 0;
+    let penny = 0;
+    let remainingChange = cents;
+
+    
+}
